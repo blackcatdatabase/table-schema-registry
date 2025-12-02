8eddd1e7f4d86a6c12c5cdd66e21a52b1bcd62ed
Vito416 <vito416@protonmail.com>
2025-12-02 19:28:18 +0100

    Sync submodule state after YAML views + upsert/coverage fixes

--- schema/040_views_joins.mysql.sql
+++ schema/040_views_joins.mysql.sql
@@ -1,7 +1,7 @@
--- Auto-generated from joins-mysql.psd1 (map@mtime:2025-11-27T17:49:37Z)
+-- Auto-generated from joins-mysql.yaml (map@94ebe6c)
 -- engine: mysql
--- view:   schema_registry_versions_latest
--- Latest version per system/component
+-- view:   schema_versions_latest
+
 CREATE OR REPLACE ALGORITHM=TEMPTABLE SQL SECURITY INVOKER VIEW vw_schema_versions_latest AS
 WITH ranked AS (
   SELECT
